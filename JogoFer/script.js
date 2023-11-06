5db46b92c6df06ac2d7887268e27f522fe38c26f
Whond <gilneidefernandaf@gmail.com>
2023-11-06 11:56:50 -0300

    Sprite run working

--- JogoFer/script.js
+++ JogoFer/script.js
@@ -5,35 +5,35 @@ const canvas_width = canvas.width = 778;
 const canvas_height = canvas.height = 625;
 
 const largura = 128;
-const altura = 128;
+const altura = 130;
 
 const background1 = document.getElementById('backImg');
 const personagem = document.querySelector("#personagem");
-
-let estado = 1;
-// let walkx = 150;
-// let walky = 0;
-
 personagem.onload = function() {
     ctx.drawImage(personagem, 0, 0);
 }
 
+let estado = 0;
+let walkx = 100;
+let walky = 0;
+
+requestAnimationFrame(animate);
+
 function animate(){
     ctx.clearRect(0, 0, canvas_width, canvas_height);
-    // ctx.drawImage(personagem, 0, 0, 128, 128, );
-    ctx.drawImage(personagem, 0, 0, largura, altura, (canvas_width/2)-largura, (canvas_height/2)-altura, altura, largura);
+
+    ctx.drawImage(personagem, estado*largura, altura, 
+                                largura, altura, 
+                                (canvas_width/2)-(largura/2), (canvas_height/2)-(altura/2), 
+                                128, 128);
 
     requestAnimationFrame(animate);
+    estado++;
+    if (estado == 3) {
+        estado = 0;
+    }
 }
 
 animate();
-
-
-
-// function draw() {
-//     ctx.clearRect(0, 0, 300, 300);
-//     ctx.drawImage(imagem, 0, 0
-//     );
-// }
 
 let rungame = setInterval(draw, 100);
