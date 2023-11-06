c0b7fdc42d957e6caa46fe998ba51e5ca43d73ff
Whond <gilneidefernandaf@gmail.com>
2023-11-06 10:56:32 -0300

    Update Game

--- JogoFer/script.js
+++ JogoFer/script.js
@@ -4,11 +4,11 @@ const ctx = canvas.getContext('2d')
 const canvas_width = canvas.width = 778;
 const canvas_height = canvas.height = 625;
 
-const largura = 101;
+const largura = 128;
 const altura = 128;
 
 const background1 = document.getElementById('backImg');
-const personagem = document.getElementById('personagem');
+const personagem = document.querySelector("#personagem");
 
 let estado = 1;
 // let walkx = 150;
@@ -20,7 +20,9 @@ personagem.onload = function() {
 
 function animate(){
     ctx.clearRect(0, 0, canvas_width, canvas_height);
-    ctx.drawImage(personagem, 0, 0, 128, 128, 0, 0, 128, 128);
+    // ctx.drawImage(personagem, 0, 0, 128, 128, );
+    ctx.drawImage(personagem, 0, 0, largura, altura, (canvas_width/2)-largura, (canvas_height/2)-altura, altura, largura);
+
     requestAnimationFrame(animate);
 }
 
